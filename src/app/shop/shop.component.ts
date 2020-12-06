41bb4b5aa1f07d48c0b48fbd83f85adba8ae87a6
hp46 <hp46@gold01.cs.calvin.edu>
2020-12-05 20:26:56 -0500

    updated

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+interface Item {
+  img_id: string;
+  goods_name: string;
+  img_src: string;
+  quant: number;
+}
 
 @Component({
   selector: 'app-shop',
@@ -6,23 +14,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  quant: number;
+  quant: number = 0;
+  goodsList: Array<Item>;
 
+  goods_name: string;
+  img_src: string;
+  img_id: string
 
-  constructor() { 
-    this.quant = 0;
+  constructor( private db: AngularFirestore ) {
+    this.db.collection<Item>('/goodsCollection').valueChanges().subscribe(res => {
+      this.goodsList = res;
+    });
+  }
+
+  storeContact(){
+    let randomID:string = Math.random().toString(36).substring(7)
+    this.db.doc<Item>(`/contactCollection/${randomID}`).set({
+      img_id: randomID,
+      img_src: this.img_src,
+      goods_name: this.goods_name,
+      quant: 0
+    })
+    console.log("store working!")
+
+    this.img_id = "";
+    this.img_src = "";
+    this.goods_name = "";
   }
 
   ngOnInit(): void {
   }
 
-  plusone() {
-    this.quant = this.quant + 1;
+  plusone(index: number) {
+    this.goodsList[index].quant += 1;
   }
 
-  minusone() {
-    if (this. quant !== 0 ) {
-      this.quant = this.quant - 1;
+  minusone(index: number) {
+    if (this.goodsList[index].quant >= 0 ) {
+      this.goodsList[index].quant -= 1;
     }
   }
 }
